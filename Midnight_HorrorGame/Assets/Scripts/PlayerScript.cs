33d97ab828372b4bc118102d57c4058984aaec78
Soe-Htet-Naung <shn.dev.portfolio.001@gmail.com>
2021-11-23 00:44:57 +0800

    added All Logic for Pray system

--- Midnight_HorrorGame/Assets/Scripts/PlayerScript.cs
+++ Midnight_HorrorGame/Assets/Scripts/PlayerScript.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    //Def System Pray
+    public bool isPraying = false; // for later use, like casting time.
+    public bool canPray = true;
+    public bool isProtectedByPray = false;
+    private float grantedPrayProtectionTime = 7f;
+    public float prayTimer = 0;
+    private float prayDuration = 4f;
+    private float prayCooldown = 10f;
 
     //Salt Circle Related Variables
     public bool isProctected = false;//set to public to see the update status in Inspector;
@@ -19,10 +27,53 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Pray();
         PTimeCount();
     }
 
-    private void PTimeCount()
+    private void Pray()
+    {
+        if(prayTimer <= 0)
+        {
+            canPray = true;
+        }
+        else if(prayTimer != 0)
+        {
+            canPray = false;
+            prayTimer -= Time.deltaTime;
+            if(prayTimer <= 0)
+            {
+                prayTimer = 0;
+                canPray = true;
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            if (canPray == true)
+            {
+                Debug.Log("is Praying");
+                prayTimer = prayCooldown;
+                Debug.Log("Prayed and Cooldown has started");
+                isProtectedByPray = true;
+                StartCoroutine("PrayProtectionTimeCount");
+            }
+            else if(canPray == false)
+            {
+                Debug.Log("can't pray 'cuz of cool down");
+            }
+        }
+
+    }
+
+    private IEnumerator PrayProtectionTimeCount()
+    {
+
+        yield return new WaitForSeconds(grantedPrayProtectionTime);
+            isProtectedByPray = false;
+        yield return null;
+    }
+        private void PTimeCount()
     {
         if(isProctected == true)
         {
