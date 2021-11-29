ac5df3bf51bfd6e2656a4876c61d69e5891ceca3
Soe-Htet-Naung <shn.dev.portfolio.001@gmail.com>
2021-11-29 21:47:44 +0800

    Documented and added a bit more lines to Debug

--- Midnight_HorrorGame/Assets/Scripts/CandleScript.cs
+++ Midnight_HorrorGame/Assets/Scripts/CandleScript.cs
@@ -6,6 +6,7 @@ public class CandleScript : MonoBehaviour
 {
     public Transform ghostPosition;
     public float distanceFromGhost = 0;
+    public float dangerousDistance = 5f;
     void Start()
     {
         
@@ -14,11 +15,16 @@ public class CandleScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CheckForGhost();
+        DetectGhost();
     }
 
-    void CheckForGhost()
+    void DetectGhost()//Again, It does what it sounds like
     {
-        distanceFromGhost = Vector3.Distance(this.transform.position, ghostPosition.position);
+        distanceFromGhost = Vector3.Distance(this.transform.position, ghostPosition.position); //Find the distance
+
+        if(distanceFromGhost <= dangerousDistance)//if the ghost is close or in dangerZone
+        {
+            Debug.Log("Chuckles! I'm in DANGERRRRR"); //replace this with what we are going to do to alarm the player that the ghost is near.
+        }
     }
 }
