9db79f646d81ea7a02df98727dfa768373ec9c17
Soe-Htet-Naung <shn.dev.portfolio.001@gmail.com>
2021-11-30 15:21:19 +0800

    Candle now flicker lights accordingly to distance from ghost

--- Midnight_HorrorGame/Assets/Scripts/CandleScript.cs
+++ Midnight_HorrorGame/Assets/Scripts/CandleScript.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class CandleScript : MonoBehaviour
 {
     public Transform ghostPosition;
+    public GameObject lightSource; // LightSource
     public float distanceFromGhost = 0;
     public float dangerousDistance = 5f;
     void Start()
     {
-        
+        lightSource.SetActive(true);
     }
 
     // Update is called once per frame
@@ -25,6 +26,11 @@ public class CandleScript : MonoBehaviour
         if(distanceFromGhost <= dangerousDistance)//if the ghost is close or in dangerZone
         {
             Debug.Log("Chuckles! I'm in DANGERRRRR"); //replace this with what we are going to do to alarm the player that the ghost is near.
+            lightSource.SetActive(false);
+        }
+        else if (distanceFromGhost > dangerousDistance)
+        {
+            lightSource.SetActive(true);
         }
     }
 }
