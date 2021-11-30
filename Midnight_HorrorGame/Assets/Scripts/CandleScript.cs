0413e825e19c53527b4229691f50910d350c740f
Soe-Htet-Naung <shn.dev.portfolio.001@gmail.com>
2021-11-30 15:27:28 +0800

    documentation

--- Midnight_HorrorGame/Assets/Scripts/CandleScript.cs
+++ Midnight_HorrorGame/Assets/Scripts/CandleScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CandleScript : MonoBehaviour
 {
     public Transform ghostPosition;
-    public GameObject lightSource; // LightSource
+    public GameObject lightSource;
     public float distanceFromGhost = 0;
     public float dangerousDistance = 5f;
     void Start()
@@ -26,11 +26,11 @@ public class CandleScript : MonoBehaviour
         if(distanceFromGhost <= dangerousDistance)//if the ghost is close or in dangerZone
         {
             Debug.Log("Chuckles! I'm in DANGERRRRR"); //replace this with what we are going to do to alarm the player that the ghost is near.
-            lightSource.SetActive(false);
+            lightSource.SetActive(false); //alarm the player with light
         }
-        else if (distanceFromGhost > dangerousDistance)
+        else if (distanceFromGhost > dangerousDistance) // If ghost is not in the dangerZone
         {
-            lightSource.SetActive(true);
+            lightSource.SetActive(true); //indicates that player is currently safe from ghost
         }
     }
 }
