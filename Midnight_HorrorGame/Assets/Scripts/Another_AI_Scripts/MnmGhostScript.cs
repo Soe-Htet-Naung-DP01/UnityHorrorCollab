f21d304b29217fff7fec030abfb058a3b6441837
Soe-Htet-Naung <shn.dev.portfolio.001@gmail.com>
2021-12-05 19:01:59 +0800

    Documentation and Instructions of what this script should do

--- Midnight_HorrorGame/Assets/Scripts/Another_AI_Scripts/MnmGhostScript.cs
+++ Midnight_HorrorGame/Assets/Scripts/Another_AI_Scripts/MnmGhostScript.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class MnmGhostScript : MonoBehaviour
 {
+    //Basic ---> Check if player is proctected by either Salt Circle or Pray.
+    //If player is protected ---> Blink away and go to other  room.
+    //P1 ---> Walk Around The Map Randomly, Stop by in some room, Enjoy the stay in the room for a while.
+    //P2 ---> Search for Player but not always.
+    //P3 ---> Destory Salt Circles and Make Pray ineffective by Chance. 
+    //      ---> Blink Positions
+
+    //Getting Variables From Another Script
+
+    //Basic Variables
+    float ghostMovementSpeed = 5f;
+    
+    //Phase One Variables
+
+    //Phase Two Variables
+
+    //Phase Three Variables
+
+
     // Start is called before the first frame update
     void Start()
     {
